6f0d944c29145b681ef2cf68182c389e28213949
ieongch0106 <jieong.dev@gmail.com>
2022-11-23 18:19:27 -0500

    reservation updated

--- dining.js
+++ dining.js
@@ -2,6 +2,121 @@ const randomizer = (n) => {
     return Math.floor(Math.random() * n) + 1;
 }
 
+// Get the modal
+const modal = document.getElementById("myModal");
+
+const modal_yes = document.getElementById("modal-yes");
+const modal_no = document.getElementById("modal-no");
+
+const next_button = document.getElementById("time-button");
+// Get the <span> element that closes the modal
+const close = document.getElementById("close");
+
+let tableId;
+
+// When the user clicks on <span> (x), close the modal
+close.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+modal_yes.onclick = function() {
+    document.getElementById("modal-title").innerHTML = "What time?";
+    document.getElementById("modal-buttons").style.display = "none";
+    document.getElementById("container-hour").style.display = "block";
+    document.getElementById("container-time").style.display = "block";
+    next_button.style.display = "block";
+}
+
+modal_no.onclick = function() {
+    modal.style.display = "none";
+}
+
+next_button.onclick = function() {
+    const space = document.createElement("br");
+    const title = document.getElementById("modal-title");
+    document.getElementById("modal-title").innerHTML = "How long?";
+    title.appendChild(space);
+    title.innerHTML += "(in minutes)"
+    document.getElementById("container-hour").style.display = "none";
+    document.getElementById("container-time").style.display = "none";
+    document.getElementById("modal-range").style.display = "block";
+    next_button.firstElementChild.innerHTML = "Confirm";
+    next_button.onclick = function () {
+        const slideValue = document.getElementById("slider").valueAsNumber;
+        document.getElementById("modal-range").style.display = "none";
+        next_button.style.display = "none";
+        title.innerHTML = `The following table: \n ${tableId}`;
+        title.appendChild(space);
+        title.innerHTML += "has been reserved successfully ";
+        const date = new Date();
+        const dateString = date.toLocaleTimeString('en-US', 
+        { hour12: false, 
+            hour: "numeric", 
+            minute: "numeric"});
+        title.innerHTML += "at " + dateString + "!";
+        title.appendChild(space);
+        const textNode = document.createElement("p");
+        textNode.classList.add("mt-4")
+        title.append(textNode);
+        var newDate = new Date(date.getTime() + slideValue*60000);
+        var deadline = newDate.getTime();
+        var x = setInterval(function() {
+        var now = new Date().getTime();
+        var t = deadline - now;
+        var hours = Math.floor((t%(1000 * 60 * 60 * 24))/(1000 * 60 * 60));
+        var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((t % (1000 * 60)) / 1000);
+        if (hours !== 0) {
+            textNode.innerHTML = hours + "h " + minutes + "m " + seconds + "s ";
+        } else {
+            textNode.innerHTML = minutes + "m " + seconds + "s ";
+        }
+            if (t < 0) {
+                clearInterval(x);
+                textNode.innerHTML = "EXPIRED";
+            }
+        }, 1000);
+    }
+}
+
+
+const hour = document.getElementById("hour");
+for (let i = 7; i < 24; i++) {
+    const li = document.createElement("li");
+    const h3 = document.createElement("h3");
+    h3.innerHTML = i;
+    li.appendChild(h3);
+    hour.appendChild(li);
+}
+
+const time = document.getElementById("time");
+for (let i = 0; i < 60; i++) {
+    const li = document.createElement("li");
+    const h3 = document.createElement("h3");
+    h3.innerHTML = i;
+    li.appendChild(h3);
+    time.appendChild(li);
+}
+
+const slideValue = document.querySelector("span");
+const inputSlider = document.querySelector("input");
+inputSlider.oninput = (()=>{
+  let value = inputSlider.value;
+  slideValue.textContent = value;
+  slideValue.style.left = (value/2) + "%";
+  slideValue.classList.add("show");
+});
+inputSlider.onblur = (()=>{
+  slideValue.classList.remove("show");
+});
+
 for (let elem of document.querySelectorAll(".hidden")) {
     const nums = elem.querySelectorAll("b");
     const randomNum = randomizer(10);
@@ -20,6 +135,8 @@ for (let elem of document.querySelectorAll(".hidden")) {
 const tableGenerator = () => {
     const random = randomizer(3);
     const table = document.createElement("button");
+    const title = document.getElementById("modal-title");
+    const yes_button = document.getElementById("modal-yes");
     if (random === 1) {
         table.style.background = "green";
         table.innerHTML = "✓";
@@ -30,6 +147,29 @@ const tableGenerator = () => {
     } else {
         table.style.background = "red";
         table.innerHTML = "X";
+    }
+    table.onclick = function() {
+        if (random === 1) {
+            title.innerHTML = "Reserve?"
+            yes_button.disabled = false;
+            yes_button.style.filter = "brightness(1)";
+            yes_button.style.cursor = "pointer";
+            
+        }
+        else if (random === 2) {
+            title.innerHTML = "The table that you selected is currently occupied."
+            yes_button.disabled = true;
+            yes_button.style.filter = "brightness(0.5)";
+            yes_button.style.cursor = "not-allowed";
+        }
+        else {
+            title.innerHTML = "The table that you selected is not available."
+            yes_button.disabled = true;
+            yes_button.style.filter = "brightness(0.5)";
+            yes_button.style.cursor = "not-allowed";
+        }
+        tableId = table.id;
+        modal.style.display = "block";
     }
     return table;
 }
